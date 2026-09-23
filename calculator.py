df1f7c6242dbc33a573e4dc019f94b95cba6ba7f
Nithyashree KS <n4175204@gmail.com>
2026-09-24 00:08:38 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,6 @@
+# Simple Calculator for Robotics Calculation
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(f"Addition: {a+b}")
+print(f"Subtraction: {a-b}")
+print(f"Multiplication: {a*b}")
